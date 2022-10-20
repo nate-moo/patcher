00f7cc7df1a3954dda0a34494512beaf83215002
nate-moo <40650681+nate-moo@users.noreply.github.com>
2022-10-20 13:44:55 -0400

    Prevent Name mangling

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "https://nate-moo.github.io/patcher/"
+  base: "https://nate-moo.github.io/patcher/",
+  esbuild: {
+    minifyIdentifiers: false,
+    minifySyntax: true,
+    minifyWhitespace: true,
+  }
 })
